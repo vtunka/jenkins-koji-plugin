3ba3b9ce0e3b5f3db504aed7276f2a4abb5232f5
Vaclav Tunka <vtunka@gmail.com>
2014-04-23 22:18:53 +0200

    Create KojiBuilder

--- src/main/java/org/jenkinsci/plugins/koji/KojiBuilder.java
+++ src/main/java/org/jenkinsci/plugins/koji/KojiBuilder.java
@@ -1,4 +1,4 @@
-package org.jenkinsci.plugins.jenkinskoji;
+package org.jenkinsci.plugins.koji;
 import hudson.Launcher;
 import hudson.Extension;
 import hudson.util.FormValidation;
@@ -21,7 +21,7 @@ import java.io.IOException;
  * <p>
  * When the user configures the project and enables this builder,
  * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
- * and a new {@link HelloWorldBuilder} is created. The created
+ * and a new {@link KojiBuilder} is created. The created
  * instance is persisted to the project configuration XML by using
  * XStream, so this allows you to use instance fields (like {@link #name})
  * to remember the configuration.
@@ -30,15 +30,15 @@ import java.io.IOException;
  * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)}
  * method will be invoked. 
  *
- * @author Kohsuke Kawaguchi
+ * @author Vaclav Tunka
  */
-public class HelloWorldBuilder extends Builder {
+public class KojiBuilder extends Builder {
 
     private final String name;
 
     // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
-    public HelloWorldBuilder(String name) {
+    public KojiBuilder(String name) {
         this.name = name;
     }
 
@@ -71,11 +71,11 @@ public class HelloWorldBuilder extends Builder {
     }
 
     /**
-     * Descriptor for {@link HelloWorldBuilder}. Used as a singleton.
+     * Descriptor for {@link KojiBuilder}. Used as a singleton.
      * The class is marked as public so that it can be accessed from views.
      *
      * <p>
-     * See <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
+     * See <tt>src/main/resources/hudson/plugins/hello_world/KojiBuilder/*.jelly</tt>
      * for the actual HTML fragment for the configuration screen.
      */
     @Extension // This indicates to Jenkins that this is an implementation of an extension point.
