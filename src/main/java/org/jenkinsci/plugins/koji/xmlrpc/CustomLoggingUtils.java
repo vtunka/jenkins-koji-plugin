d834e6dd19265fdcb6c448ad7f9d6accbaf4d540
Vaclav Tunka <vtunka@gmail.com>
2014-05-07 01:25:46 +0200

    Enable logging and debugging of incoming and outgoing XML-RPC request and responses

---
+++ src/main/java/org/jenkinsci/plugins/koji/xmlrpc/CustomLoggingUtils.java
@@ -0,0 +1,86 @@
+package org.jenkinsci.plugins.koji.xmlrpc;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.io.IOUtils;
+import org.apache.xmlrpc.XmlRpcException;
+import org.slf4j.Logger;
+
+public class CustomLoggingUtils {
+
+    public static void logRequest(Logger logger,
+                                  RequestEntity requestEntity) throws XmlRpcException {
+        ByteArrayOutputStream bos = null;
+        try {
+            logger.debug("---- Request ----");
+            bos = new ByteArrayOutputStream();
+            requestEntity.writeRequest(bos);
+            logger.debug(toPrettyXml(logger, bos.toString()));
+        } catch (IOException e) {
+            throw new XmlRpcException(e.getMessage(), e);
+        } finally {
+            IOUtils.closeQuietly(bos);
+        }
+    }
+
+    public static void logRequest(Logger logger, String content) {
+        logger.debug("---- Request ----");
+        logger.debug(toPrettyXml(logger, content));
+    }
+
+    public static String logResponse(Logger logger, InputStream istream)
+            throws XmlRpcException {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(istream));
+            String line = null;
+            StringBuilder respBuf = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                respBuf.append(line);
+            }
+            String response = respBuf.toString();
+            logger.debug("---- Response ----");
+            logger.debug(toPrettyXml(logger, respBuf.toString()));
+            return response;
+        } catch (IOException e) {
+            throw new XmlRpcException(e.getMessage(), e);
+        } finally {
+            IOUtils.closeQuietly(reader);
+        }
+    }
+
+    public static void logResponse(Logger logger, String content) {
+        logger.debug("---- Response ----");
+        logger.debug(toPrettyXml(logger, content));
+    }
+
+    private static String toPrettyXml(Logger logger, String xml) {
+        try {
+            Transformer transformer =
+                    TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(
+                    "{http://xml.apache.org/xslt}indent-amount", "2");
+            StreamResult result = new StreamResult(new StringWriter());
+            StreamSource source = new StreamSource(new StringReader(xml));
+            transformer.transform(source, result);
+            return result.getWriter().toString();
+        } catch (Exception e) {
+            logger.warn("Can't parse XML");
+            return xml;
+        }
+    }
+}
---
+++ src/main/java/org/jenkinsci/plugins/koji/xmlrpc/CustomXmlRpcCommonsTransportFactory.java
@@ -0,0 +1,60 @@
+package org.jenkinsci.plugins.koji.xmlrpc;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcCommonsTransport;
+import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
+import org.apache.xmlrpc.client.XmlRpcTransport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomXmlRpcCommonsTransportFactory extends
+        XmlRpcCommonsTransportFactory {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public CustomXmlRpcCommonsTransportFactory(XmlRpcClient pClient) {
+        super(pClient);
+    }
+
+    @Override
+    public XmlRpcTransport getTransport() {
+        return new LoggingTransport(this);
+    }
+
+    private class LoggingTransport extends XmlRpcCommonsTransport {
+
+        public LoggingTransport(CustomXmlRpcCommonsTransportFactory pFactory) {
+            super(pFactory);
+        }
+
+        /**
+         * Logs the request content in addition to the actual work.
+         */
+        @Override
+        protected void writeRequest(final ReqWriter pWriter) throws XmlRpcException {
+            super.writeRequest(pWriter);
+            if (logger.isDebugEnabled()) {
+                CustomLoggingUtils.logRequest(logger, method.getRequestEntity());
+            }
+        }
+
+        /**
+         * Logs the response from the server, and returns the contents of
+         * the response as a ByteArrayInputStream.
+         */
+        @Override
+        protected InputStream getInputStream() throws XmlRpcException {
+            InputStream istream = super.getInputStream();
+            if (logger.isDebugEnabled()) {
+                return new ByteArrayInputStream(
+                        CustomLoggingUtils.logResponse(logger, istream).getBytes());
+            } else {
+                return istream;
+            }
+        }
+    }
+}
