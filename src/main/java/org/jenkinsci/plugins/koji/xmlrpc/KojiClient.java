c476c4983106410a4ab433d955956e580c9ae368
Vaclav Tunka <vtunka@gmail.com>
2014-05-09 19:35:31 +0200

    Place inner classes at the end of the KojiClient file

--- src/main/java/org/jenkinsci/plugins/koji/xmlrpc/KojiClient.java
+++ src/main/java/org/jenkinsci/plugins/koji/xmlrpc/KojiClient.java
@@ -31,6 +31,7 @@ public class KojiClient {
 
     /**
      * Get the KojiClient Singleton instance.
+     *
      * @param kojiInstanceURL URL of remote Koji instance.
      */
     public static KojiClient getKojiClient(String kojiInstanceURL) {
@@ -48,6 +49,7 @@ public class KojiClient {
 
     /**
      * Gets latest builds.
+     *
      * @param tag Koji tag
      * @param pkg Koji package
      * @return Map of properties for latest build.
@@ -83,6 +85,7 @@ public class KojiClient {
 
     /**
      * Retrieves information about a given build.
+     *
      * @param buildId BuildId can be Name-Version-Release (NVR) or numeric buildId.
      * @return A map containing all information about given build.
      */
@@ -135,12 +138,13 @@ public class KojiClient {
 
     /**
      * Gets information about logged user.
+     *
      * @return Session identification.
      */
     public String getSession() {
         String result = null;
         try {
-            result = (String) koji.execute("showSession", new Object[] {});
+            result = (String) koji.execute("showSession", new Object[]{});
         } catch (XmlRpcException e) {
             e.printStackTrace();
         }
@@ -181,6 +185,59 @@ public class KojiClient {
         }
 
 //        return null;
+    }
+
+    /**
+     * Greet the remote Koji instance and test the communication.
+     *
+     * @return
+     */
+    public String sayHello() {
+        StringBuilder sb = new StringBuilder();
+        try {
+            Object result = koji.execute("hello", new Object[]{"Hello"});
+            sb.append("Jenkins-Koji Plugin: Hello Koji server running at " + kojiInstanceURL);
+            sb.append("\nKoji: " + result);
+
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * Connect to remote Koji instance. Uses custom Transport factory adding a None / null support for XML-RPC.
+     *
+     * @param kojiInstanceURL Address of the remote Koji server.
+     * @return XMLRPC client instance.
+     */
+    private XmlRpcClient connect(String kojiInstanceURL) {
+        XmlRpcClient koji = new XmlRpcClient();
+        koji.setTransportFactory(new XmlRpcCommonsTransportFactory(koji));
+        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+        koji.setTransportFactory(new XmlRpcCommonsTransportFactory(koji));
+        koji.setTypeFactory(new MyTypeFactory(koji));
+        config.setEnabledForExtensions(true);
+        config.setEnabledForExceptions(true);
+
+        try {
+            config.setServerURL(new URL(kojiInstanceURL));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        koji.setConfig(config);
+
+        return koji;
+    }
+
+    public void setDebug(boolean debug) {
+        if (debug)
+            koji.setTransportFactory(new MyXmlRpcCommonsTransportFactory(koji));
+        else
+            koji.setTransportFactory(new XmlRpcCommonsTransportFactory(koji));
     }
 
     /**
@@ -273,56 +330,5 @@ public class KojiClient {
         BuildParams build() {
             return new BuildParams(id, tag, latest, pkg, owner, type);
         }
-    }
-
-    /**
-     * Greet the remote Koji instance and test the communication.
-     * @return
-     */
-    public String sayHello() {
-        StringBuilder sb = new StringBuilder();
-        try {
-            Object result = koji.execute("hello", new Object[] {"Hello"});
-            sb.append("Jenkins-Koji Plugin: Hello Koji server running at " + kojiInstanceURL);
-            sb.append("\nKoji: " + result);
-
-            return sb.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    /**
-     * Connect to remote Koji instance. Uses custom Transport factory adding a None / null support for XML-RPC.
-     * @param kojiInstanceURL Address of the remote Koji server.
-     * @return XMLRPC client instance.
-     */
-    private XmlRpcClient connect(String kojiInstanceURL) {
-        XmlRpcClient koji = new XmlRpcClient();
-        koji.setTransportFactory(new XmlRpcCommonsTransportFactory(koji));
-        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
-        koji.setTransportFactory(new XmlRpcCommonsTransportFactory(koji));
-        koji.setTypeFactory(new MyTypeFactory(koji));
-        config.setEnabledForExtensions(true);
-        config.setEnabledForExceptions(true);
-
-        try {
-            config.setServerURL(new URL(kojiInstanceURL));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        koji.setConfig(config);
-
-        return koji;
-    }
-
-    public void setDebug(boolean debug) {
-        if (debug)
-            koji.setTransportFactory(new MyXmlRpcCommonsTransportFactory(koji));
-        else
-            koji.setTransportFactory(new XmlRpcCommonsTransportFactory(koji));
     }
 }
